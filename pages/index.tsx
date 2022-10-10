fd3f6c54ec4a4004df413106bd8b5118c383cab6
Albert Mazur <kontakt@mazuralbert.pl>
2022-10-10 14:55:06 +0200

    add styles in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <div />
+    <div className="flex flex-col min-h-screen">
+      <header className="max-w-md mx-auto w-full">
+        <nav className="bg-gray-500 text-white px-4 py-2">
+          Nawigacja
+        </nav>
+      </header>
+      <main className="flex-grow max-w-md mx-auto w-full">
+        Main
+      </main>
+      <footer className="bg-gray-900 max-w-md mx-auto w-full text-white px-4 py-2">
+        Stopka
+      </footer>
+    </div>
   )
 }
