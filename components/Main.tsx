f8674cf1ca11bb6f4e5d759c611866bf8e72f9d1
Albert Mazur <kontakt@mazuralbert.pl>
2022-10-12 10:17:53 +0200

    update ssg page

--- components/Main.tsx
+++ components/Main.tsx
@@ -6,8 +6,8 @@ interface MainProps {
 
 export const Main = ({children}: MainProps) => {
   return (
-    <main className="flex-grow max-w-2xl mx-auto grid p-6 gap-6 sm:grid-cols-2">
-    {/*<main className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">*/}
+    // <main className="flex-grow max-w-2xl mx-auto grid p-6 gap-6 sm:grid-cols-2">
+    <main className="flex-grow mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       {children}
     </main>
   );
--- pages/products.tsx
+++ pages/products.tsx
@@ -1,14 +1,29 @@
-import {Header} from "../components/Header";
-import {Main} from "../components/Main";
-import {Footer} from "../components/Footer";
-import {InferGetStaticPropsType} from "next";
+import { Header } from "../components/Header";
+import { Main } from "../components/Main";
+import { Footer } from "../components/Footer";
+import { InferGetStaticPropsType } from "next";
+import {Product} from "../components/Product";
 
-export default function ProductsPage({data}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function ProductsPage({ data }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Main>
-        {data[0].title}
+        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+          {data.map(product => {
+            return <li key={product.id} className="shadow-xl border-2">
+              <Product
+                data={{
+                  title: product.title,
+                  description: product.description,
+                  thumbnailUrl: product.image,
+                  thumbnailAlt: product.title,
+                  rating: product.rating.rate,
+                }}
+              />
+            </li>
+          })}
+        </ul>
       </Main>
       <Footer />
     </div>
