b57cb348d7d8eb76dee4a949764e322036b5fa5b
Albert Mazur <kontakt@mazuralbert.pl>
2022-10-12 11:22:06 +0200

    add title, clean up StoreApiResponse interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import {Main} from "../components/Main";
 import {Product} from "../components/Product";
 
 const DATA = {
+  title: 'Pies',
   description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis incidunt ipsam labore, magni quos unde. A, adipisci alias, consequatur distinctio facilis modi nisi nulla, officiis quidem quo quos sint unde ut vitae voluptatum. Accusamus aspernatur facilis maxime recusandae repellendus. Ex itaque qui quibusdam repellat! Dolorem eius illum recusandae voluptatibus.`,
   thumbnailUrl: `https://picsum.photos/id/237/536/354`,
   thumbnailAlt: `Czarny pies patrzy na ciebie`,
--- pages/products.tsx
+++ pages/products.tsx
@@ -42,14 +42,14 @@ export const getStaticProps = async () => {
 };
 
 export interface StoreApiResponse {
-  id:          number;
-  title:       string;
-  price:       number;
+  id: number;
+  title: string;
+  price: number;
   description: string;
-  category:    string;
-  image:       string;
-  rating:      {
-    rate:  number;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
     count: number;
   }
 }
