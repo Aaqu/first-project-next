2e81ff3313ce49a942b05d0098661380df34293b
Aaqu <kontakt@mazuralbert.pl>
2022-10-13 14:55:34 +0200

    footer auto year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="bg-emerald-800 mx-auto w-full text-white text-center px-4 py-2">
-      &#169; Od 2022 | mazuralbert.pl
+      &#169; {(new Date).getFullYear()} | mazuralbert.pl
     </footer>
   );
 };
