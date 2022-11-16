15f039dab1ef2cc04a82b4f26065c2d29bd881dd
Albert Mazur <kontakt@mazuralbert.pl>
2022-11-16 08:21:01 +0100

    update migration

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -3,7 +3,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const apolloClient = new ApolloClient({
   uri: process.env.GRAPHQL_URL,
   cache: new InMemoryCache(),
-
 });
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,67 @@
 import {apolloClient} from "../graphql/apolloClient";
 import {ProductListItem} from "../components/Product";
-import {GetProductListDocument, GetProductListQuery} from "../generated/graphql";
+import {
+  CreateProductReviewDocument,
+  CreateProductReviewMutation, CreateProductReviewMutationVariables,
+  GetProductListDocument,
+  GetProductListQuery, useCreateProductReviewMutation
+} from "../generated/graphql";
 import {InferGetStaticPropsType} from "next";
-import {add} from "unload";
+// {data}: InferGetStaticPropsType<typeof getStaticProps>
+export default function Home() {
+  const [createReview, {data, loading, error}] = useCreateProductReviewMutation()
 
-export default function Home({data}: InferGetStaticPropsType<typeof getStaticProps>) {
-  const addReview = () => {
-    console.log("add"); // 5:07
+  const addReview = async () => {
+    const response = await createReview ({
+      variables: {
+        review: {
+          headline: "next app comment",
+          name: "test",
+          email: "test@a.a",
+          content: "opinia",
+          rating: 5
+        }
+      }
+    })
+
+    console.log(response)
   }
 
   return (
     <>
-      <h1 className="font-bold text-2xl pt-12 pb-3">NEW!</h1>
-      <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
-        {data.products.map(product => {
-          return <li key={product.id} className="shadow-xl border-2">
-            <ProductListItem
-              data={{
-                id: product.id,
-                title: product.name,
-                price: product.price,
-                thumbnailUrl: product.images[0].url,
-                thumbnailAlt: product.name,
-              }}
-            />
-          </li>
-        })}
-      </ul>
-      <button type="button" onClick={addReview}>Dodaj komentarz</button>
+      {/*<h1 className="font-bold text-2xl pt-12 pb-3">NEW!</h1>*/}
+      {/*<ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">*/}
+      {/*  {data.products.map(product => {*/}
+      {/*    return <li key={product.id} className="shadow-xl border-2">*/}
+      {/*      <ProductListItem*/}
+      {/*        data={{*/}
+      {/*          id: product.id,*/}
+      {/*          title: product.name,*/}
+      {/*          price: product.price,*/}
+      {/*          thumbnailUrl: product.images[0].url,*/}
+      {/*          thumbnailAlt: product.name,*/}
+      {/*        }}*/}
+      {/*      />*/}
+      {/*    </li>*/}
+      {/*  })}*/}
+      {/*</ul>*/}
+      <button type="button" onClick={addReview} className="border border-gray-600 p-2 ">Dodaj komentarz</button>
+      {loading && <div>Ładowanie..</div>}
+      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+
     </>
   )
 }
 
-export const getStaticProps = async () => {
-  const {data} = await apolloClient.query<GetProductListQuery>({
-    query: GetProductListDocument,
-  });
-
-  return {
-    props: {
-      data,
-    },
-  };
-};
+// export const getStaticProps = async () => {
+//   const {data} = await apolloClient.query<GetProductListQuery>({
+//     query: GetProductListDocument,
+//   });
+//
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
